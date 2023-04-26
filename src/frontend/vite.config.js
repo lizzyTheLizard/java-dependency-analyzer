bd391067da86df8c5f02f3ff30893c665f580d5a
lizzythelizad <14069652+lizzythelizard@users.noreply.github.com>
2023-04-26 02:59:26 +0200

    Reorder git repo

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -4,21 +4,21 @@ import {viteSingleFile} from 'vite-plugin-singlefile';
 import {resolve} from 'path';
 
 export default defineConfig({
-  plugins: [viteSingleFile()],
-  base: '',
-  build: {
-    chunkSizeWarningLimit: '2000 KiB',
-    rollupOptions: {
-      external: [
-        'src/data.json',
-      ]
-    }
-  },
-  resolve: {
-    alias: {
-      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
+    plugins: [viteSingleFile()],
+    base: '',
+    build: {
+        chunkSizeWarningLimit: '2000 KiB',
+        rollupOptions: {
+            external: [
+                'src/data.json',
+            ]
+        }
+    },
+    resolve: {
+        alias: {
+            '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
 
 
